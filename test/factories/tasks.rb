7f7e8b676fa5a00b1ec79e9a8e6eddaceb0f152f
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-25 20:17:07 +0700

    fix typo: changed assignee associationg from manager to developer

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     expired_at
 
     association :author, factory: :manager
-    association :assignee, factory: :manager
+    association :assignee, factory: :developer
   end
 end
