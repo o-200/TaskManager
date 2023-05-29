66780031f7998bcb970c7df117793d758beea92d
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-29 21:19:49 +0700

    route for session was moved to right place

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root :to => "web/boards#show"
 
-  resource :session, only: [:new, :create, :destroy]
-
   scope module: :web do
     resource :board, only: :show
-    resource :session, only: :new
+    resource :session, only: [:new, :create, :destroy]
   end
 end
