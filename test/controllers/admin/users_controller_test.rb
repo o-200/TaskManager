a66ccc448b87c977f95000161a09d49da84b860e
Alexander Abramov <snlyshigoto@gmail.com>
2023-05-31 19:40:20 +0700

    added destroy action

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -41,4 +41,9 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert_response :redirect
   end
 
+  test "should delete destroy" do
+    user = create(:user)
+    delete :destroy, params: {id: user.id }
+    assert_response :redirect
+  end
 end
