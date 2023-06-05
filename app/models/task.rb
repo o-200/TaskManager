a399cfa994f98aa920ed1dfc6fd174e63eb1a19e
Alexander Abramov <snlyshigoto@gmail.com>
2023-06-05 15:14:35 +0700

    added api functionality

--- app/models/task.rb
+++ app/models/task.rb
@@ -31,4 +31,8 @@ class Task < ApplicationRecord
       transition in_ready_for_release: :released
     end
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["assignee_id", "author_id", "created_at", "description", "expired_at", "id", "name", "state", "updated_at"]
+  end
 end
