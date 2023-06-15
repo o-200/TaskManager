75539666028109ca86871520d4c99a57c041d381
Alexander Abramov <snlyshigoto@gmail.com>
2023-06-15 20:14:09 +0700

    added console receiving messages for docker ip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << '.app.uffizzi.com'
+
+  config.web_console.permissions = '10.0.2.2'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
